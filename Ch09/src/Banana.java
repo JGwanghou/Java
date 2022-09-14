570f8fd44a1dac81544ef3ed3e5ac6f67e7184a3
JGwanghou <jogwangho0619@gmail.com>
2022-09-14 14:48:07 +0900

    2022/09/14

---
+++ Ch09/src/Banana.java
@@ -0,0 +1,22 @@
+
+
+public class Banana {
+	
+	private String country;
+	private int price;
+	
+	public Banana(String country, int price) {
+		this.country = country;
+		this.price = price;
+	}
+	
+	@Override
+	public String toString() {
+		System.out.println("객체번호: " + hashCode()); // 객체와 식별번호(해시값)
+		System.out.println("원산지 : " + country);
+		System.out.println("가격 : " + price);
+		
+		return super.toString();
+	}
+}
+
