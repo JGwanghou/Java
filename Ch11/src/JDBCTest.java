c2cb651fae0320173263523fa887ddac1bfdc86b
JGwanghou <jogwangho0619@gmail.com>
2022-09-15 17:03:18 +0900

    20220915

--- Ch11/src/JDBCTest.java
+++ Ch11/src/JDBCTest.java
@@ -14,27 +14,27 @@ import java.sql.DriverManager;
 public class JDBCTest {
 	public static void main(String[] args) {
 		// DB정보
-		String host = "jdbc:mysql://127.0.0.1:3306/java2DB";
+		String host = "jdbc:mysql://127.0.0.1:3306/java2db";
 		String user = "root";
 		String pass = "1234";
 		
 		try {
-		// 1단계 - JDBC 드라이버 로드
-		Class.forName("com.mysql.cj.jdbc.Driver");
+			// 1단계 - JDBC 드라이버 로드
+			Class.forName("com.mysql.cj.jdbc.Driver");
+				
+			// 2단계 - 데이터베이스 접속
+			Connection conn = DriverManager.getConnection(host,user,pass);
 			
-		// 2단계 - 데이터베이스 접속
-		Connection conn = DriverManager.getConnection(host,user,pass);
-		
-		if(conn == null) {
-			System.out.println("데이터베이스 접속 성공!");
-		}else {
-			System.out.println("데이터베이스 접속 실패!");
+			if(conn != null) {
+				System.out.println("데이터베이스 접속 성공!");
+			}else {
+				System.out.println("데이터베이스 접속 실패!");
+			}
+			
+			
+		}catch (Exception e) {
+			e.printStackTrace();
 		}
-		
-		
-	}catch (Exception e) {
-		e.printStackTrace();
-	}
 		System.out.println("데이터베이스 접속 테스트 완료");
 		
 	}
