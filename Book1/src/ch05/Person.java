821b72cd88029637bc5be1085bfd49ca1609a7cc
JGwanghou <jogwangho0619@gmail.com>
2022-09-22 15:04:55 +0900

    20220922

--- Book1/src/ch05/Person.java
+++ Book1/src/ch05/Person.java
@@ -2,4 +2,9 @@ package ch05;
 
 public class Person {
 
+	String name;
+	int height;
+	double weight;
+	char gender;
+	boolean married;
 }
--- Book1/src/ch05/Student.java
+++ Book1/src/ch05/Student.java
@@ -1,5 +1,25 @@
 package ch05;
 
 public class Student {
-
+	int studentID;
+	String studentName;
+	int grade;
+	String address;
+	
+	public void showStudentInfo() {
+		System.out.println(studentName + "," + address);
+	}	
+	public String getStudentName() {
+		return studentName;
+	}	
+		
+	public static void main(String[] args) {
+		Student studentAhn = new Student();
+		studentAhn.studentName = "안연수";
+		
+		System.out.println(studentAhn.studentName);
+		System.out.println(studentAhn.getStudentName());
+	}
+	
 }
+
